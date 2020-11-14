de1374fe13c20cde81f91f4e579bb6fc260fe995
wlsr <leonardo.siangas@ceis.cs.umss.edu.bo>
2020-11-13 22:16:26 -0400

    Se añadio menu de opciones

---
+++ app/src/main/java/com/lsr/petagram/FormularioContacto.java
@@ -0,0 +1,14 @@
+package com.lsr.petagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormularioContacto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_contacto);
+    }
+}
--- app/src/main/java/com/lsr/petagram/MainActivity.java
+++ app/src/main/java/com/lsr/petagram/MainActivity.java
@@ -1,14 +1,18 @@
 package com.lsr.petagram;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -51,6 +55,28 @@ public class MainActivity extends AppCompatActivity {
         listaMascotas.setLayoutManager(llm);
         inicalizarListaMascotas();
         inicializarAdaptador();
+    }
+    // menu de opciones
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_opcion, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+       switch  (item.getItemId()){
+           case R.id.mContact:
+               Intent intent = new Intent(this, FormularioContacto.class);
+               startActivity(intent);
+           break;
+           case R.id.mAbout:
+               Intent intent1 = new Intent(this, About.class);
+               startActivity(intent1);
+           break;
+       }
+        return super.onOptionsItemSelected(item);
     }
 
     public MascotaAdaptador adaptador;
